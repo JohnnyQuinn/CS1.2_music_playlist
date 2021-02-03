07bdafabbe084f27d818f3b4af0475dfb9f8c164
johnnyquinn <jftq97@gmail.com>
2021-02-03 10:26:46 -0500

    completed song.opy

--- Playlist.py
+++ Playlist.py
@@ -8,27 +8,58 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
+    new_song = Song(title)
+
+    if self.__first_song == None:
+      self.__first_song = new_song
+    else: 
+      new_song = Song(title)
+      if self.__first_song.get_next_song() == None:
+        self.__first_song.set_next_song(new_song) 
+
 
 
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    current_song = self.__first_song
+    index = 0
+    while current_song != None:
+      index += 1
+      if current_song == title:
+        return index
+      else:
+        current_song = current_song.get_next_song()
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    pass
+    current_song = self.__first_song
+
+    while current_song != None:
+
+      if current_song.get_next_song() == title:
+        current_song.set_next_song(current_song.get_next_song().get_next_song())
+        return 
+      else:
+        current_song = current_song.get_next_song()
 
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    pass
+    current_song = self.__first_song
+    counter = 0
+
+    while current_song != None:
+      current_song = current_song.get_next_song()
+      counter += 1
+    
+    return counter
+
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
